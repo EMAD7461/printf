6041231349630f9a0f9da1482c7062aa128d6f33
EMAD7461 <emad7461@gmail.com>
2023-08-22 15:25:01 -0700

    handle

--- write_handlers.c
+++ write_handlers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+/************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
  * @c: char types.
@@ -43,7 +44,9 @@ int handle_write_char(char c, char buffer[],
 	return (write(1, &buffer[0], 1));
 }
 
+/************************* WRITE NUMBER *************************/
 /**
+ * write_number - Prints a string
  * @is_negative: Lista of arguments
  * @ind: char types.
  * @buffer: Buffer array to handle print
@@ -133,7 +136,6 @@ int write_num(int ind, char buffer[],
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
-
 
 /**
  * write_unsgnd - Writes an unsigned number
@@ -248,4 +250,3 @@ int write_pointer(char buffer[], int ind, int length,
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
-
